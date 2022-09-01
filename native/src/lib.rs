f9c0a238f4865dc7a564598b0ac6da92c9af04ae
Varun Oberoi <varun@rubird.com>
2022-09-01 11:55:07 +0530

    debugging docker image

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -11,6 +11,8 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("hello", hello)?;
     cx.export_function("p1_launch_server", party1::launch_server)?;
+    cx.export_function("p1_launch_server_regular", party1::launch_server_regular)?;
+
     cx.export_function("p1_ecdsa_get_child_share", party1::ecdsa::get_child_share)?;
 
     cx.export_function("p2_ecdsa_generate_master_key", party2::ecdsa::generate_master_key)?;
