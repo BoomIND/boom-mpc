20dc36fcdd7a1b85ba782947e7099e51274b4731
Varun Oberoi <varun@rubird.com>
2022-09-08 18:07:58 +0530

    debugging

---
+++ clear-gh-cache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gh api -H "Accept: application/vnd.github+json" \
+        /repos/boomll/mpc/actions/caches \
+        | for ID in `jq '.actions_caches[].id'`; \
+          do echo "Deleting $ID"; \
+            gh api --method DELETE /repos/boomll/mpc/actions/caches/$ID
+            done
