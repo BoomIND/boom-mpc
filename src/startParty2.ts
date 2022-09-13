dd56a28d84c05891a6e3a247f23a7c4bccde9209
Varun Oberoi <varun@rubird.com>
2022-09-13 12:57:05 +0530

    debugging

--- src/startParty2.ts
+++ src/startParty2.ts
@@ -1,4 +1,7 @@
 import ServerlessHttp from "serverless-http";
 import app from './party2-express';
 
-export const handler = ServerlessHttp(app);
+export const handler = ServerlessHttp(app, {
+  binary: false,
+  provider: "aws"
+});
