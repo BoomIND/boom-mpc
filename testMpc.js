39ed4d615f7a735483a255ff14c7ca031066e0d7
Varun Oberoi <varun@rubird.com>
2022-09-14 11:25:32 +0530

    debugging

--- testMpc.js
+++ testMpc.js
@@ -3,20 +3,28 @@ var baseUrl = "https://j33cxwpvof.execute-api.ap-south-1.amazonaws.com/dev";
 
 const main = async () => {
   try {
-    const { id, publicKey } = (
-      await axios.post(`${baseUrl}/generateKey`, {})
-    ).data;
-    console.log(`generateKey ${id}, ${publicKey}`)
-    const publicKeyResp = (
-      await axios.post(`${baseUrl}/fetchPublicKey`, {
-        keyId: id,
-      })
-    ).data;
-    console.log(`fetchedKey ${publicKeyResp.id}, ${publicKeyResp.publicKey}`)
+    let id, publicKey;
+    if (!process.env.KEY_ID) {
+      const r = (
+        await axios.post(`${baseUrl}/generateKey`, {})
+      ).data;
+      console.log(`generateKey ${r.id}, ${r.publicKey}`)
+      id = r.id
+      publicKey = r.publicKey
+    } else {
+      const publicKeyResp = (
+        await axios.post(`${baseUrl}/fetchPublicKey`, {
+          keyId: process.env.KEY_ID,
+        })
+      ).data;
+      console.log(`fetchedKey ${publicKeyResp.id}, ${publicKeyResp.publicKey}`)
+      id = publicKeyResp.id
+      publicKey = publicKeyResp.publicKey
+    }
     for (var i =0; i<10; i++) {
       const sign = (
         await axios.post(`${baseUrl}/sign`, {
-          keyId: 'f83eb11d-4436-4579-b0c9-e8cece3d8a02',
+          keyId: id,
           msg: `5d9f06235dc98bf33efda3d575c8f8be0301fd73ab14a16f63c0623ed4ec741${i}`,
         })
       ).data;
